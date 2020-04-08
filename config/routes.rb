13b6b716c9a53785891df32dd11b775b7847c371
Teerth Patel <teerth1205@gmail.com>
2020-04-08 22:01:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :session, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   root 'application#hello'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
