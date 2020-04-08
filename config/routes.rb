eb708b0e3e2ba69e992ded5364758ba1b8778de2
Teerth Patel <teerth1205@gmail.com>
2020-04-08 21:33:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions only: [:new]
+  root 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
