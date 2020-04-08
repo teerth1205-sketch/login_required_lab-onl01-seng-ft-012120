9a022d2dbf786be22a9462159113b5a6da9cae2f
Teerth Patel <teerth1205@gmail.com>
2020-04-08 22:12:30 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,7 +3,9 @@ class SecretsController < ApplicationController
   before_action :require_login
   
   def show 
-  
+   if current_user.nil?
+     redirect_to '/login'
+   end 
   end 
   
   private
