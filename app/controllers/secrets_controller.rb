19c8cd6c1cafe534b3ab7558658dd759e6bbe300
Teerth Patel <teerth1205@gmail.com>
2020-04-08 22:09:43 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,14 @@
 class SecretsController < ApplicationController
   
-  before_action 
+  before_action :require_login
+  
   def show 
   
   end 
+  
+  private
+  
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
 end 
